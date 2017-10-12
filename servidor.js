351c20f2d726a45ae50443b2b0291c087c277448
3absente <victordiazracero@gmail.com>
2017-10-12 15:28:37 +0200

    Creación de funcionalidad en el cliente
    Se han creado los métodos para poder darle funcionalidad al cliente:
    conexión de usuario, envío de mensaje, actualización de lista de usuarios, desconexión de usuario.

--- servidor.js
+++ servidor.js
@@ -1,39 +1,49 @@
-var express = require('express');
+var express = require('express'); 
 var http = require('http');
 var socket = require('socket.io');
 
-var app= express();
+var app = express();
 var server = http.createServer(app);
-var usuarios=[];
+var usuarios = [];
 
 server.listen(3000, function(){
-	console.log("Servidor corriendo en el puerto: 3000");
+    console.log("Corriendo en el puerto 3000");
 })
 
 var io = socket.listen(server);
 
-app.get('/', function(req,res){
-	res.sendFile(__dirname + '/cliente.html');
-})
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/cliente.html');
+});
 
-io.on('connection', function(socket)){
-	socket.on('nuevo usuario', function(usuario,callback){
-		if(usuarios.indexOf(usuario) != -1){
-			callback(false);
-		}else{
-			callback(true);
-			socket.usuario = usuario;
-			usuarios.push(usuario);
-			actualizarUsuarios();
-		}
-	})
+io.on('connection', function(socket){
+    socket.on('nuevo usuario', function(usuario, callback){
+        if(usuarios.indexOf(usuario) != -1){
+            callback(false);
+        }
+        else{
+            callback(true);
+            socket.usuario = usuario;
+            usuarios.push(usuario);
+            actualizarUsuarios();
+            io.emit('mensaje',{mensaje: 'se ha conectado', usuario: socket.usuario});
+        }
+    });
+    
+    socket.on('nuevo mensaje', function(mensaje){
+        io.emit('mensaje', {mensaje: mensaje, usuario: socket.usuario});
+    });
+    
+    function actualizarUsuarios(){
+        io.emit('actualizarUsuarios', usuarios);
+    };
+    
+    socket.on('disconnect', function(data){
+        usuarios.splice(usuarios.indexOf(socket.usuario), 1);
+        io.emit('mensaje',{mensaje: 'se ha desconectado', usuario: socket.usuario});
+        actualizarUsuarios();
+    });
+    
 
-	socket.on('nuevo mensaje', function(mensaje){
-		io.emit('mensaje', {mensaje: mensaje, usuario: socket.usuario});
-	})
-
-	function actualizarUsuarios(){
-		io.emit('actualizarUsuarios', usuarios);
-	}
 })
 
